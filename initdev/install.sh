d0fd6211ebd18fc02861989dbc2bf4b873d585fa
Soumiak <37780467+Soumiak@users.noreply.github.com>
2018-04-05 23:53:37 +0200

    Update install.sh

---
+++ initdev/install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Author: kheladi soumia
+# Banner
+cat << "EOF"
+ _       _ _      _                     _ _   
+|_ | _ () |_ _| | ____   _    _ () |_ 
+ | || '_ \| | __/ _` |/ _ \ \ / /   / _` | | __|
+ | || | | | | || (| |  __/\ V /   | (_| | | | 
+|___|_| |_|_|\__\__,_|\___| \_/   ()_, |_|\__|
+                                    |___/       
+by soumia kheladi -USTHB 2018-
+
+EOF
+
+# input data
+github_user='soumiak'
+
+# Check if command exist
+if command -v initdev &> /dev/null; then
+echo 'the application is already installed !'
+exit 0
+else
+
+echo 'installing initdev ::'
+# cloning the repo
+if [ ! -d $HOME/.initdev ]; then
+echo 'Cloning the repot ::'
+git clone https://github.com/$github_user/initdev_cc/ $HOME/.initdev
+mv $HOME/.initdev/main.sh $HOME/.initdev/initdev
+else
+	echo ".initdev folder already exist !"
+	exit 1
+fi
+
+# configuring Linux
+if [ $SHELL == '/bin/bash' ]; then
+echo 'bash detected => initializing PATH variable.'
+echo "export PATH=$PATH:$HOME/.initdev" >> $HOME/.bashrc
+echo 'resourcing bashrc ::'
+source $HOME/.bashrc
+fi
+
+if [ $SHELL == '/bin/bash' ]; then
+echo 'zsh detected => initializing PATH variable.'
+echo "export PATH=$PATH:$HOME/.initdev" >> $HOME/.zshrc
+echo 'resourcing zshrc ::'
+source $HOME/.zshrc
+fi
+
+echo 'Installation terminé.'
+
+fi
